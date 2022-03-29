73b95a144e5e11d3ee006cd4ad54ff10addd455b
Ygor Pereira de Sá <80071063+ygorxds@users.noreply.github.com>
2022-03-28 22:14:27 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+const dataAtual = new Date();
+
+const dataFormatada = dataAtual.toLocaleDateString(
+  'pt-BR',
+  {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  }
+);
+
+console.log(dataFormatada);
